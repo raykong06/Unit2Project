63f1640a7f55d1af048c598c3533dbe5d38cc44a
BT_4N2_24 <rkong2588@bths.edu>
2022-10-12 12:23:58 -0400

    finished user input and parsing of first coordinate in runner class

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
         // Instance Variables
+        Scanner s = new Scanner(System.in);
+
         int x1;
         int y1;
         int x2;
@@ -23,6 +26,24 @@ public class EquationRunner {
         double x3;
 
         // User Input
+        System.out.println("Enter the first (x,y) coordinate point: ");
+        coord1 = s.nextLine();
+
+        System.out.println("Enter the second (x,y) coordinate point: ");
+        coord2 = s.nextLine();
+
+        // Parse Out Values
+            // parse x1 value
+        indexComma1 = coord1.indexOf(",");
+        stringX1 = coord1.substring(1,indexComma1);
+        x1 = Integer.parseInt(stringX1);
+
+            // parse y1 value
+        lengthCoord1 = coord1.length();
+        stringY1 = coord1.substring((indexComma1 + 1),lengthCoord1);
+        y1 = Integer.parseInt(stringY1);
+
+
 
         LinearEquation one = new LinearEquation(7,5,2,2);
         System.out.println(one.toString());
